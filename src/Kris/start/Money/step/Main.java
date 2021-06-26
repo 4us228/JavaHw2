34171ccec1e42c36257982b4bfffd39138b7bcb9
4us228 <4us.in.ua@gmail.com>
2021-06-26 13:47:18 +0300

    Initial commit

---
+++ src/Kris/start/Money/step/Main.java
@@ -0,0 +1,14 @@
+package Kris.start.Money.step;
+
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    static Random rm = new Random();
+
+    public static void main(String[] args) {
+
+    }
+
+}
